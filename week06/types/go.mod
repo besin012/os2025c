6dee2ae56ad9fa8374b6775fe4f755aa0462837c
이지윤 <wldbs6988@naver.com>
2025-09-29 17:28:35 +0900

    v0.1 types/main.go

---
+++ week06/types/go.mod
@@ -0,0 +1,3 @@
+module github.com/besin012/types
+
+go 1.25.1
---
+++ week06/types/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println(reflect.TypeOf(2.31))
+	fmt.Println(reflect.TypeOf("Kim Inha"))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf('A'))
+	fmt.Println(reflect.TypeOf(19))
+
+}
