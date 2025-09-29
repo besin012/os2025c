90e53c8e89b247356523c23dcf873d3b7f9685de
이지윤 <wldbs6988@naver.com>
2025-09-29 17:54:24 +0900

    v0.5 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -6,6 +6,19 @@ import (
 )
 
 func main() {
+	//zero values
+	var f64 float64
+	var t bool
+	var s string
+	var i int
+	var i16 int16
+
+	fmt.Println(f64, reflect.TypeOf(f64))
+	fmt.Println(t, reflect.TypeOf(t))
+	fmt.Println(s, reflect.TypeOf(s))
+	fmt.Println(i, reflect.TypeOf(i))
+	fmt.Println(i16, reflect.TypeOf(i16))
+
 	//var id int16
 	//var name string
 	//var gpa float32
@@ -18,12 +31,16 @@ func main() {
 	//var name string = "Kim Inha"
 	//var gpa float32 = 3.99
 
-	var id = 999
-	var name = "Kim Inha"
-	var gpa = 3.99
+	//var id = 999
+	//var name = "Kim Inha"
+	//var gpa = 3.99
 
-	fmt.Println("학번은", id, reflect.TypeOf(id), ",이름은", name, reflect.TypeOf(name))
-	fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
+	//id := 999  //단축형태 선언 기호
+	//name := "Kim Inha"
+	//gpa := 3.99
+
+	//fmt.Println("학번은", id, reflect.TypeOf(id), ",이름은", name, reflect.TypeOf(name))
+	//fmt.Println("평점 :", gpa, reflect.TypeOf(gpa))
 
 	//fmt.Println(reflect.TypeOf(2.31))
 	//fmt.Println(reflect.TypeOf("Kim Inha"))
