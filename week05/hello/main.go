fd0a72e6217481ece83a59f3c0f470483b242b84
이지윤 <wldbs6988@naver.com>
2025-09-29 18:14:16 +0900

    gitignore

--- week05/hello/main.go
+++ week05/hello/main.go
@@ -6,6 +6,7 @@ func main() {
 	//name := "Go"
 	//fmt.Println("hello", name)
 
+	
 	number := 7
 	fmt.Printf("Number is %d.\n", number)
 }
--- week06/types/main.go
+++ week06/types/main.go
@@ -6,18 +6,24 @@ import (
 )
 
 func main() {
-	//zero values
 	var f64 float64
-	var t bool
-	var s string
-	var i int
-	var i16 int16
+	totalPrice := 1000
 
+	fmt.Println(totalPrice)
 	fmt.Println(f64, reflect.TypeOf(f64))
-	fmt.Println(t, reflect.TypeOf(t))
-	fmt.Println(s, reflect.TypeOf(s))
-	fmt.Println(i, reflect.TypeOf(i))
-	fmt.Println(i16, reflect.TypeOf(i16))
+
+	//zero values
+	//var f64 float64
+	//var t bool
+	//var s string
+	//var i int
+	//var i16 int16
+
+	//fmt.Println(f64, reflect.TypeOf(f64))
+	//fmt.Println(t, reflect.TypeOf(t))
+	//fmt.Println(s, reflect.TypeOf(s))
+	//fmt.Println(i, reflect.TypeOf(i))
+	//fmt.Println(i16, reflect.TypeOf(i16))
 
 	//var id int16
 	//var name string
