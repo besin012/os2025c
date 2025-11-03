5a265ae15b72066b7ec8513767d6f9e30d288303
besin0124 <wldbs6988@naver.com>
2025-11-03 18:18:00 +0900

    week10 array v0.3

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	//"reflect"
 )
 
 func main() {
@@ -12,12 +12,21 @@ func main() {
 	// arrayInt[1]++
 	// arrayInt[1] = arrayInt[1] + 1
 	// fmt.Println(arrayInt[1]) // zero value + 2
-	arrayBool := [3]bool{true, false, true}
-	var arrayInt [3]int
-	fmt.Println(reflect.TypeOf(arrayBool))
-	fmt.Printf("%#v\n", arrayBool) //
 
-	fmt.Println(arrayBool[1])
-	arrayInt[1] = 2
-	fmt.Println(arrayInt[1])
+	// arrayBool := [3]bool{true, false, true} // array literal
+	// var arrayInt [3]int
+	// fmt.Println(reflect.TypeOf(arrayBool))
+	// fmt.Printf("%#v\n", arrayBool) //[3]bool{true, false, true}
+
+	// fmt.Println(arrayBool[1])
+	// arrayInt[1] = 2
+	// fmt.Println(arrayInt[1])
+
+	arrayBool := [2]bool{true, false}
+	arrayInt := [3]int{-9, 11, 7}
+	for i := 0; i < 3; i++ {
+		fmt.Println(i, arrayBool[i])
+		fmt.Println(i, arrayInt[i])
+	}
+
 }
---
+++ week10/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "week10/pkg/greeting" // custom package
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
