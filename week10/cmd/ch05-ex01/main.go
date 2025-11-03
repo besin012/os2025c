ec3b5e98343084d440983623d1ca5d41a9b475cf
besin0124 <wldbs6988@naver.com>
2025-11-03 18:19:00 +0900

    week10 array v0.4

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -24,7 +24,8 @@ func main() {
 
 	arrayBool := [2]bool{true, false}
 	arrayInt := [3]int{-9, 11, 7}
-	for i := 0; i < 3; i++ {
+	//for i := 0; i < 3; i++ {
+	for i := 0; i < len(arrayInt); i++ {
 		fmt.Println(i, arrayBool[i])
 		fmt.Println(i, arrayInt[i])
 	}
